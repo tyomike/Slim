8e725027bc7373e790a6fc5e54b75b0bfbe2bd34
Михаил <jcat@MacBook-Air-jcat-4.local>
2022-03-15 15:06:45 +0300

    slim

--- public/index.php
+++ public/index.php
@@ -1,10 +1,12 @@
 <?php
 
 // Подключение автозагрузки через composer
+namespace App;
 require __DIR__ . '/../vendor/autoload.php';
-
 use Slim\Factory\AppFactory;
 use DI\Container;
+$app = AppFactory::create();
+
 
 $container = new Container();
 $container->set('renderer', function () {
@@ -16,6 +18,7 @@ $app->addErrorMiddleware(true, true, true);
 
 
 $app->get('/users', function ($request, $response) {
+
     $term = $request->getQueryParam('term');
     $users = ['lisa', 'mishel', 'adel', 'keks', 'kamila'];
 
@@ -31,6 +34,34 @@ $app->get('/users', function ($request, $response) {
     $params = ['term' => $term,
                'users' => $users
         ];
-    return $this->get('renderer')->render($response, 'users/index1.phtml', $params);
+});
+
+
+$app->get('/users/new', function ($request, $response) {
+    $params = [
+        'user' => ['name' => '', 'email' => '', 'password' => '', 'passwordConfirmation' => '', 'city' => ''],
+        'errors' => []
+    ];
+    return $this->get('renderer')->render($response, $params);
+})->setName('authorization');
+
+//
+$app->post('/users/new', function ($request, $response){
+
+    $user = $request->getParsedBodyParam('user');
+
+    $new = json_encode($user);
+    file_put_contents('info.json', $new);
+
+    return $this->get('renderer')->render($response, "users/authorization.phtml");
+});
+
+// Получаем роутер – объект отвечающий за хранение и обработку маршрутов
+$router = $app->getRouteCollector()->getRouteParser();
+// Не забываем прокинуть его в обработчик
+$app->get('/', function ($request, $response) use ($router) {
+    // в функцию передаётся имя маршрута, а она возвращает url
+    // остальной код
+    return $response->withRedirect($router->urlFor('authorization')); // /users/new)
 });
 $app->run();
